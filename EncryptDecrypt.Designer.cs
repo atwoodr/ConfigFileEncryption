84677850e044a117671069368dc68817432a017c
Ryan Atwood <ryan.atwood@assurant.com>
2018-06-13 11:00:42 -0500

    Initial commit

---
+++ EncryptDecrypt.Designer.cs
@@ -0,0 +1,206 @@
+﻿using System.Windows.Forms;
+
+namespace EncryptDecryptAppConfigFile
+{
+    partial class EncryptDecrypt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEncryption = new System.Windows.Forms.TextBox();
+            this.cmdOpen = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.allLikeConfigFilesOptionGroupBox = new System.Windows.Forms.GroupBox();
+            this.radioButton_Yes = new System.Windows.Forms.RadioButton();
+            this.radioButton_No = new System.Windows.Forms.RadioButton();
+            this.cmdDecrypt = new System.Windows.Forms.Button();
+            this.cmdEncrypt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.allLikeConfigFilesOptionGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDilog";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtEncryption);
+            this.panel1.Controls.Add(this.cmdOpen);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 70);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Open .NET Config File";
+            // 
+            // txtEncryption
+            // 
+            this.txtEncryption.Location = new System.Drawing.Point(15, 38);
+            this.txtEncryption.Name = "txtEncryption";
+            this.txtEncryption.Size = new System.Drawing.Size(445, 20);
+            this.txtEncryption.TabIndex = 6;
+            // 
+            // cmdOpen
+            // 
+            this.cmdOpen.Location = new System.Drawing.Point(466, 38);
+            this.cmdOpen.Name = "cmdOpen";
+            this.cmdOpen.Size = new System.Drawing.Size(22, 20);
+            this.cmdOpen.TabIndex = 5;
+            this.cmdOpen.Text = "&..";
+            this.cmdOpen.UseVisualStyleBackColor = true;
+            this.cmdOpen.Click += new System.EventHandler(this.cmdOpen_Click_1);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.allLikeConfigFilesOptionGroupBox);
+            this.panel2.Controls.Add(this.cmdDecrypt);
+            this.panel2.Controls.Add(this.cmdEncrypt);
+            this.panel2.Location = new System.Drawing.Point(12, 88);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(500, 134);
+            this.panel2.TabIndex = 6;
+            // 
+            // allLikeConfigFilesOptionGroupBox
+            // 
+            this.allLikeConfigFilesOptionGroupBox.Controls.Add(this.radioButton_Yes);
+            this.allLikeConfigFilesOptionGroupBox.Controls.Add(this.radioButton_No);
+            this.allLikeConfigFilesOptionGroupBox.Location = new System.Drawing.Point(27, 42);
+            this.allLikeConfigFilesOptionGroupBox.Name = "allLikeConfigFilesOptionGroupBox";
+            this.allLikeConfigFilesOptionGroupBox.Size = new System.Drawing.Size(288, 73);
+            this.allLikeConfigFilesOptionGroupBox.TabIndex = 4;
+            this.allLikeConfigFilesOptionGroupBox.TabStop = false;
+            this.allLikeConfigFilesOptionGroupBox.Text = "Encrypt/Decrypt all .config files in the same directory?";
+            // 
+            // radioButton_Yes
+            // 
+            this.radioButton_Yes.AutoSize = true;
+            this.radioButton_Yes.Location = new System.Drawing.Point(13, 42);
+            this.radioButton_Yes.Name = "radioButton_Yes";
+            this.radioButton_Yes.Size = new System.Drawing.Size(257, 17);
+            this.radioButton_Yes.TabIndex = 1;
+            this.radioButton_Yes.TabStop = true;
+            this.radioButton_Yes.Text = "Yes, modify each .config file in the same directory";
+            this.radioButton_Yes.UseVisualStyleBackColor = true;
+            this.radioButton_Yes.CheckedChanged += new System.EventHandler(this.radioButtonYes_CheckedChanged);
+            // 
+            // radioButton_No
+            // 
+            this.radioButton_No.AutoSize = true;
+            this.radioButton_No.Checked = true;
+            this.radioButton_No.Location = new System.Drawing.Point(13, 19);
+            this.radioButton_No.Name = "radioButton_No";
+            this.radioButton_No.Size = new System.Drawing.Size(209, 17);
+            this.radioButton_No.TabIndex = 0;
+            this.radioButton_No.TabStop = true;
+            this.radioButton_No.Text = "No, only modify the selected .config file";
+            this.radioButton_No.UseVisualStyleBackColor = true;
+            this.radioButton_No.CheckedChanged += new System.EventHandler(this.radioButtonNo_CheckedChanged);
+            // 
+            // cmdDecrypt
+            // 
+            this.cmdDecrypt.Location = new System.Drawing.Point(137, 13);
+            this.cmdDecrypt.Name = "cmdDecrypt";
+            this.cmdDecrypt.Size = new System.Drawing.Size(104, 23);
+            this.cmdDecrypt.TabIndex = 3;
+            this.cmdDecrypt.Text = "&Decrypt Config";
+            this.cmdDecrypt.UseVisualStyleBackColor = true;
+            this.cmdDecrypt.Click += new System.EventHandler(this.cmdDecrypt_Click_1);
+            // 
+            // cmdEncrypt
+            // 
+            this.cmdEncrypt.Location = new System.Drawing.Point(27, 13);
+            this.cmdEncrypt.Name = "cmdEncrypt";
+            this.cmdEncrypt.Size = new System.Drawing.Size(104, 23);
+            this.cmdEncrypt.TabIndex = 2;
+            this.cmdEncrypt.Text = "&Encrypt Config";
+            this.cmdEncrypt.UseVisualStyleBackColor = true;
+            this.cmdEncrypt.Click += new System.EventHandler(this.cmdEncrypt_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Modify all .config files in this folder?";
+            // 
+            // EncryptDecrypt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 235);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "EncryptDecrypt";
+            this.Text = "Encrypt Decrypt Connection String";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.allLikeConfigFilesOptionGroupBox.ResumeLayout(false);
+            this.allLikeConfigFilesOptionGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private Panel panel1;
+        private Label label1;
+        private TextBox txtEncryption;
+        private Button cmdOpen;
+        private Panel panel2;
+        private Button cmdDecrypt;
+        private Button cmdEncrypt;
+
+        private Label label2;
+        protected RadioButton selectedRadioButton;
+        private GroupBox allLikeConfigFilesOptionGroupBox;
+        private RadioButton radioButton_Yes;
+        protected RadioButton radioButton_No;
+    }
+}
+
---
+++ EncryptDecrypt.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Configuration;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace EncryptDecryptAppConfigFile
+{
+    public partial class EncryptDecrypt : Form
+    {
+        // Using constants here instead of config file settings so that we can deploy this tool as a single .exe file not dependant on .config files
+        private static readonly string EncryptionProviderName = "PFEncryptionProvider";
+        private static readonly string FileType = "*.config";
+
+        private string FileName = string.Empty;
+        private static bool isUsingXDTNamespace = false;
+
+        public EncryptDecrypt()
+        {
+            InitializeComponent();
+
+            // Set default radio button choice
+            this.selectedRadioButton = radioButton_No;
+        }
+
+        public static void EncryptConfigSections(bool encrypt, List<string> configFileNames, bool saveChangesToConfigFile = true)
+        {
+            if (configFileNames != null)
+            {
+                foreach (string configFileName in configFileNames)
+                {
+                    EncryptConfigSections(encrypt, configFileName, saveChangesToConfigFile);
+                }
+            }
+        }
+
+        public static void EncryptConfigSections(bool encrypt, string configFileName, bool saveChangesToConfigFile = true)
+        {
+            try
+            {
+                // Prep the <configProtectedData> section for encryption / decryption with this tool
+                RemoveConfigProtectedDataXDTTransformXMLAttribute(configFileName);
+
+                // Open the configuration file for use
+                ExeConfigurationFileMap configMap = new ExeConfigurationFileMap { ExeConfigFilename = configFileName };
+                Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+
+                // Initialize the list of config sections that will be encrypted/decrypted
+                List<ConfigurationSection> configSections = InitializeConfigSections(configuration);
+
+                foreach (ConfigurationSection configSection in configSections)
+                {
+                    if (!configSection.ElementInformation.IsLocked && !configSection.SectionInformation.IsLocked)
+                    {
+                        if (encrypt && !configSection.SectionInformation.IsProtected)
+                        {
+                            // Encrypt the config file section
+                            configSection.SectionInformation.ProtectSection(EncryptionProviderName);
+                        }
+
+                        if (!encrypt && configSection.SectionInformation.IsProtected)
+                        {
+                            // Decrypt the config file section
+                            configSection.SectionInformation.UnprotectSection();
+                        }
+
+                        configSection.SectionInformation.ForceSave = true;
+                    }
+                }
+
+                if (saveChangesToConfigFile)
+                {
+                    // Save the current configuration
+                    configuration.Save(ConfigurationSaveMode.Modified);
+                }
+
+                // Revert the initial changes to the <configProtectedData> section if required
+                if (isUsingXDTNamespace)
+                {
+                    ReinstateConfigProtectedDataXDTTransformXMLAttribute(configFileName);
+                }
+
+                OpenConfigFileForViewing(configuration.FilePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private static List<ConfigurationSection> InitializeConfigSections(Configuration configuration)
+        {
+            List<ConfigurationSection> configSections = new List<ConfigurationSection>();
+            ConnectionStringsSection connectionStringsSection = configuration.GetSection("connectionStrings") as ConnectionStringsSection;
+            ConfigurationSection protectedAppSettingsSection = configuration.GetSection("protectedAppSettings");
+
+            if (connectionStringsSection != null)
+                configSections.Add(connectionStringsSection);
+
+            if(protectedAppSettingsSection != null)
+                configSections.Add(protectedAppSettingsSection);
+
+            return configSections;
+        }
+        
+        /// <summary>
+        /// This method prepares the config file for encryption or decryption by temporarily removing XML attributes that the 
+        /// System.Configuration library cannot deal with while encrypting/decrypting.
+        /// 
+        /// This includes 
+        /// 1) Removing any 'xmlns:xdt="http://schemas.microsoft.com/XML-Document-Transform"' attributes in the root node
+        /// 2) Removing any 'xdt:Transform="Replace"' attributes from the <configProtectedData> XML section
+        /// </summary>
+        /// <param name="configFileName"></param>
+        private static void RemoveConfigProtectedDataXDTTransformXMLAttribute(string configFileName)
+        {
+            XNamespace xdt = "http://schemas.microsoft.com/XML-Document-Transform";
+
+            // Initialize the config file as an XML document
+            XDocument xdoc = XDocument.Load(configFileName);
+            
+            // Check the root node for the XML namespace definition attribute
+            XAttribute rootNodeXmlnsAttr = xdoc.Root.Attributes(XNamespace.Xmlns + "xdt")?.FirstOrDefault();
+
+            // Check the <configProtectedData> XML section for the "xdt" XML attribute
+            XAttribute configProtectedDataXDTAttr = xdoc.Root.Element("configProtectedData").Attributes(xdt + "Transform")?.FirstOrDefault();
+
+            if (rootNodeXmlnsAttr != null && configProtectedDataXDTAttr != null)
+            {
+                // Recognize that this config file is using the XDT namespace
+                isUsingXDTNamespace = true;
+
+                // First, we need to loop through the sections of the config that we are not going to encrypt and temporarily 
+                // add the xml namespace attribute for XDT so that each line in the config file doesn't have the definition auto-added as such
+                // Note: This would not be necessary if we could suppress or override the XObjectEvent Changed event, but this appears to be impossible
+                List<XElement> sectionsToAddXmlns = xdoc.Root.Elements().Where(section => section.Name.LocalName != "configProtectedData").ToList();
+
+                // Iterate through each section and temporarily add the xml namespace attribute
+                foreach (XElement section in sectionsToAddXmlns)
+                {
+                    if (section != null && section is XElement)
+                    {
+                        section.Add(new XAttribute(XNamespace.Xmlns + "xdt", xdt));
+                    }
+                }
+
+                // Remove the 'xmlns:xdt="http://schemas.microsoft.com/XML-Document-Transform"' attribute from the configuration node
+                rootNodeXmlnsAttr.Remove();
+
+                // Now remove the <connectionStrings> and <protectedAppSettings> sections' attributes
+                xdoc.Root.Element("connectionStrings").RemoveAttributes();
+                xdoc.Root.Element("protectedAppSettings").RemoveAttributes();
+
+                // Remove the xdt:Transform attribute from the <configProtectedData> section of the config file
+                configProtectedDataXDTAttr.Remove();
+
+                // Save the file before continuing
+                xdoc.Save(configFileName);
+            }
+        }
+
+        /// <summary>
+        /// This method reinstates the XML attributes removed by the RemoveConfigProtectedDataXDTTransformXMLAttribute() method.
+        /// (To be used once the encryption/decryption process has completed)
+        /// </summary>
+        /// <param name="configFileName"></param>
+        private static void ReinstateConfigProtectedDataXDTTransformXMLAttribute(string configFileName)
+        {
+            XNamespace xdt = "http://schemas.microsoft.com/XML-Document-Transform";
+
+            // Initialize the config file as an XML document
+            XDocument xdoc = XDocument.Load(configFileName);
+            
+            // Reinstate the xmlns attribute on the configuration root node
+            xdoc.Root.Add(new XAttribute(XNamespace.Xmlns + "xdt", xdt));
+
+            // Find every section where we need to remove the temporarily added xml namespace attribute
+            List<XElement> sectionsWithXmlns = xdoc.Root.Elements().Where(section => section.Name.LocalName != "connectionStrings"
+                                                                                  && section.Name.LocalName != "protectedAppSettings"
+                                                                                  && section.Name.LocalName != "configProtectedData").ToList();
+
+            // Remove the temporarily added xml namespace attribute for each of these sections
+            sectionsWithXmlns.Attributes().Where(a => a.Name.LocalName == "xdt")?.Remove();
+
+            // Reinstate the xdt:Transform attribute from the <configProtectedData> section of the config file
+            XAttribute xdtTransformReplace = new XAttribute(xdt + "Transform", "Replace");
+
+            XElement connectionStringsNode = xdoc.Root.Element("connectionStrings");
+            XElement protectedAppSettingsNode = xdoc.Root.Element("protectedAppSettings");
+            XElement configProtectedDataNode = xdoc.Root.Element("configProtectedData");
+
+            connectionStringsNode.Add(xdtTransformReplace);
+            protectedAppSettingsNode.Add(xdtTransformReplace);
+            configProtectedDataNode.Add(xdtTransformReplace);
+
+            // Save the file
+            xdoc.Save(configFileName);
+
+            // Clear the flag
+            isUsingXDTNamespace = false;
+        }
+
+        /// <summary>
+        /// Attempts to open the file in notepad++, but opens in notepad of notepad++ isn't installed
+        /// </summary>
+        /// <param name="filePath"></param>
+        private static void OpenConfigFileForViewing(string filePath)
+        {
+            try
+            {
+                Process.Start("notepad++.exe", filePath);
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    Process.Start("notepad.exe", filePath);
+                }
+                catch { }
+            }
+        }
+
+        private void ChangeFilesEncryption(bool encrypt, bool saveChangesToConfigFile = true)
+        {
+            if (File.Exists(FileName))
+            {
+                if (this.selectedRadioButton.Name.Contains("Yes"))
+                {
+                    List<string> configFilesInSelectedFolder = GetConfigFilenamesInSelectedFolder(FileName);
+                    EncryptConfigSections(encrypt, configFilesInSelectedFolder, saveChangesToConfigFile);
+                }
+                else
+                {
+                    EncryptConfigSections(encrypt, FileName, saveChangesToConfigFile);
+                }
+            }
+            else
+            {
+                MessageBox.Show("File doesn't exist");
+            }
+        }
+
+        private List<string> GetConfigFilenamesInSelectedFolder(string fileName)
+        {
+            string directoryPath = Path.GetDirectoryName(fileName);
+            List<string> configFilesInFolder = new List<string>(Directory.GetFiles(directoryPath, FileType));
+
+            // Exclude packages.config from the list
+            configFilesInFolder.RemoveAll(PackagesConfigPredicate);
+
+            // Exclude *.Debug.config and *.Release.config from the list
+            configFilesInFolder.RemoveAll(DebugConfigPredicate);
+            configFilesInFolder.RemoveAll(ReleaseConfigPredicate);
+
+            return configFilesInFolder;
+        }
+
+        private static bool PackagesConfigPredicate(String s)
+        {
+            return s.ToLower().EndsWith("packages.config");
+        }
+
+        private static bool DebugConfigPredicate(String s)
+        {
+            return s.ToLower().EndsWith(".debug.config");
+        }
+
+        private static bool ReleaseConfigPredicate(String s)
+        {
+            return s.ToLower().EndsWith(".release.config");
+        }
+
+        private void cmdOpen_Click_1(object sender, EventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void OpenFile()
+        {
+            // This produces ".config files|*.config" if the config file setting = "*.config"
+            openFileDialog1.Filter = FileType.Substring(1) + " files|" + FileType;
+
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+            FileName = openFileDialog1.FileName;
+            txtEncryption.Text = FileName;
+        }
+
+        private void cmdEncrypt_Click_1(object sender, EventArgs e)
+        {
+            // Encrypt
+            ChangeFilesEncryption(true);
+        }
+
+        private void cmdDecrypt_Click_1(object sender, EventArgs e)
+        {
+            // Decrypt
+            ChangeFilesEncryption(false);
+        }
+
+        private void radioButtonNo_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButtonChecked(sender as RadioButton);
+        }
+
+        private void radioButtonYes_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButtonChecked(sender as RadioButton);
+        }
+
+        private void radioButtonChecked(RadioButton rbSender)
+        {
+            if (rbSender == null)
+            {
+                MessageBox.Show("Sender is not a RadioButton");
+                return;
+            }
+
+            if (rbSender.Checked)
+            {
+                // Keep track of the selected RadioButton by saving a reference to it.
+                selectedRadioButton = rbSender;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
